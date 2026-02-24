5dd7816b23243b54ff529e2f2b75785ca0ab74b3
danielosagie341 <danielosagie341@gmail.com>
2026-02-24 11:51:32 +0100

    social media post update

--- models/BlogPost.js
+++ models/BlogPost.js
@@ -1,37 +1,46 @@
 const mongoose = require('mongoose');
 
 const BlogPostSchema = new mongoose.Schema({
-  type: {
-    type: String,
-    required: true,
-    enum: ['upcoming-event', 'ongoing-activity']
-  },
-  department: {
-    type: String,
-    required: true
-  },
-  title: {
-    type: String,
-    required: true
-  },
-  description: {
-    type: String,
-    required: true
-  },
-  shortDescription: {
-    type: String
-  },
-  timestamp: {
-    type: Date,
-    default: Date.now
-  },
-  image: {
-    type: String
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
+    type: {
+        type: String,
+        required: true,
+        enum: ['upcoming-event', 'ongoing-activity', 'social-media-post']
+    },
+    department: {
+        type: String,
+        required: function() { return this.type !== 'social-media-post'; }
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    url: {
+        type: String,
+        required: function() { return this.type === 'social-media-post'; }
+    },
+    platform: {
+        type: String,
+        enum: ['twitter', 'instagram', 'youtube', 'tiktok'],
+        required: function() { return this.type === 'social-media-post'; }
+    },
+    description: {
+        type: String,
+        required: function() { return this.type !== 'social-media-post'; }
+    },
+    shortDescription: {
+        type: String
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now
+    },
+    image: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 module.exports = mongoose.model('BlogPost', BlogPostSchema);
