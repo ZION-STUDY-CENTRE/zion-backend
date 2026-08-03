653bbd123db4107eff1fefe3405c4f4e8d7b9622
JohnJay10 <jaiyeolah.john@gmail.com>
2026-08-03 09:35:54 +0100

    Updated

--- models/BlogPost.js
+++ models/BlogPost.js
@@ -1,46 +1,54 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const BlogPostSchema = new mongoose.Schema({
-    type: {
-        type: String,
-        required: true,
-        enum: ['upcoming-event', 'ongoing-activity', 'social-media-post']
+  type: {
+    type: String,
+    required: true,
+    enum: ["upcoming-event", "ongoing-activity", "social-media-post"],
+  },
+  department: {
+    type: String,
+    required: function () {
+      return this.type !== "social-media-post";
     },
-    department: {
-        type: String,
-        required: function() { return this.type !== 'social-media-post'; }
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: function () {
+      return this.type === "social-media-post";
     },
-    title: {
-        type: String,
-        required: true
+  },
+  platform: {
+    type: String,
+    enum: ["facebook", "instagram", "linkedin", "youtube", "tiktok"],
+    required: function () {
+      return this.type === "social-media-post";
     },
-    url: {
-        type: String,
-        required: function() { return this.type === 'social-media-post'; }
+  },
+  description: {
+    type: String,
+    required: function () {
+      return this.type !== "social-media-post";
     },
-    platform: {
-        type: String,
-        enum: ['facebook', 'instagram', 'linkedin', 'youtube', 'tiktok'],
-        required: function() { return this.type === 'social-media-post'; }
-    },
-    description: {
-        type: String,
-        required: function() { return this.type !== 'social-media-post'; }
-    },
-    shortDescription: {
-        type: String
-    },
-    timestamp: {
-        type: Date,
-        default: Date.now
-    },
-    image: {
-        type: String
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
+  },
+  shortDescription: {
+    type: String,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+  image: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-module.exports = mongoose.model('BlogPost', BlogPostSchema);
+module.exports = mongoose.model("BlogPost", BlogPostSchema);
